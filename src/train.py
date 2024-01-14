1d0c45ffcc4eff67e744a9fb2488ca573b65dd49
Alvaro Deras <ad2226@cornell.edu>
2024-01-14 02:08:21 -0500

    Change loss function, optimizer

--- src/train.py
+++ src/train.py
@@ -19,14 +19,14 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 train_dataloader, test_dataloader, class_names = data_setup.setup_data()
 
 def main():
-    model = model_builder.PetCNN(
+    model = model_builder.WasteClassifier(
         input_shape=3,
         hidden_units=HIDDEN_UNITS,
         output_shape=len(class_names)
     ).to(device)
 
-    loss_function = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(params=model.parameters(),
+    loss_function = torch.nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam(params=model.parameters(),
                                 lr=LEARNING_RATE)
 
     engine.train(model=model,
