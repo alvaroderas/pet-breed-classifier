324f4e03d0979ec9c54a7182700a6233e74ee365
Alvaro Deras <ad2226@cornell.edu>
2024-01-15 02:10:35 -0500

    Add dataset navigation

--- src/data_setup.py
+++ src/data_setup.py
@@ -5,18 +5,38 @@ the creation of PyTorch DataLoaders.
 Author: Alvaro Deras
 Date: January 13, 2024
 """
-import os, torchvision
+import os, requests, zipfile
 
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+from pathlib import Path
 
 NUM_WORKERS = os.cpu_count()
 BATCH_SIZE = 32
 
 data_root = "../dataset"
 
+data_path = Path("../data/")
+image_path = data_path / "waste"
 
-def setup_data(data_root: str="../dataset",
+if image_path.is_dir():
+    print(f"{image_path} directory exists.")
+else:
+    print(f"Did not find {image_path} directory, creating one...")
+    image_path.mkdir(parents=True, exist_ok=True)
+
+with open(data_path / "archive.zip", "wb") as f:
+    request = requests.get("https://www.kaggle.com/datasets/techsash/waste-classification-data/download?datasetVersionNumber=1")
+    print("Downloading waste dataset...")
+    f.write(request.content)
+
+with zipfile.ZipFile(data_path / "archive.zip", "r") as zip_ref:
+    print("Unzipping waste classification data...") 
+    zip_ref.extractall(image_path)
+
+os.remove(data_path / "archive.zip")
+
+def setup_data(data_root: str="../data/waste",
                batch_size: int=BATCH_SIZE,
                num_workers: int=NUM_WORKERS):
     """
